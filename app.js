f076aae7405608124bb96619c6be2d0384772f73
acareaga <aaroncareaga@gmail.com>
2016-01-08 08:56:51 -0700

    Added download, trying to extract images to repo

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 var Xray = require("x-ray");
+var fs = require("fs");
+var Download = require('download');
+
 var xray = new Xray();
 
-xray('http://turing.io', 'title')
+xray('http://turing.io', 'img',
+    [{
+      img: '',
+      src: '@src',
+      width: '@width',
+      height: '@height'
+    }])
     .write('results.json');
+
+(function(err, results){
+  var download = new Download()
+  results = results.filter(function(image){
+    return img.width > 40;
+  }).forEach(function(image){
+    download.get(img.src);
+  });
+  download.dest('./images');
+  download.run();
+  fs.writeFile('./results.json', JSON.stringify(results, null, '\t'));
+})
