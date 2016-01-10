bbc774e54955bef11fb39e28be648c00fd73bd20
acareaga <aaroncareaga@gmail.com>
2016-01-09 17:59:03 -0700

    Practice scraping HN with advanced selectors

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
 var Xray = require("x-ray");
-var fs = require("fs");
-var Download = require('download');
 
 var xray = new Xray();
 
-xray('http://turing.io', 'img',
+xray('https://news.ycombinator.com/', '.athing',
     [{
-      img: '',
-      src: '@src',
-      width: '@width',
-      height: '@height'
-    }])
-    .write('results.json');
-
-(function(err, results){
-  var download = new Download()
-  results = results.filter(function(image){
-    return img.width > 40;
-  }).forEach(function(image){
-    download.get(img.src);
-  });
-  download.dest('./images');
-  download.run();
-  fs.writeFile('./results.json', JSON.stringify(results, null, '\t'));
-})
+        rank: '.rank',
+        title: 'td:nth-child(3) a',
+        link: 'td:nth-child(3) a@href'
+    }]
+)
+    .paginate('a[rel="nofollow"]:last-child@href')
+    .limit(3)
+    .write('./results.json');
