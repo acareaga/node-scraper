800498d1d0163b06040f6329249f8aef3d3ad69f
acareaga <aaroncareaga@gmail.com>
2016-01-10 18:04:38 -0700

    Practice scraping with nightmare.

---
+++ server.js
@@ -0,0 +1,10 @@
+import Nightmare from "nightmare";
+
+new Nightmare()
+    .goto('http://weather.com')
+    .evaluate(function(){
+        return document.querySelector('.temperature').innerText;
+    }, function(temperature){
+        console.log(temperature);
+    })
+    .run();
